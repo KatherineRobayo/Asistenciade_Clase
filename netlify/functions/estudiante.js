28f5f183c427082481df5babb13a2f6447490b49
Katherine Robayo <akatherinerobayo@ucundinamarca.edu.co>
2025-06-03 10:11:30 -0500

    error corregido de estudiante.js

--- netlify/functions/estudiante.js
+++ netlify/functions/estudiante.js
@@ -1,12 +1,13 @@
+
+
 require("dotenv").config(); // debe estar al inicio
 const express = require("express");
 const serverless = require("@netlify/functions");
-const estudianteRoutes = require("../../Backend/routes/estudiantesRoutes"); // <- debe ser "../../routes", NO "controllers"
+const estudiantesRoutes = require("../../Backend/routes/estudiantesRoutes");
 
 const app = express();
 app.use(express.json());
 app.use("/.netlify/functions/estudiante", estudiantesRoutes);
 
 module.exports.handler = serverless.handler(app);
-
 
